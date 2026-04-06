3e0b6d0b52a06b39d62172a8bae7d9a5456a1b1d
manojtharindu11 <thilakarathnawatm.21@uom.lk>
2026-04-06 14:59:50 +0530

    Refactor OrderService to include session ID handling and improve order management logic

--- app/order_service.py
+++ app/order_service.py
@@ -1,9 +1,11 @@
 from typing import List, Optional
 from fastapi.responses import JSONResponse
+from app.generic_helper import extracting_session_id
+from app.generic_helper import get_str_from_food_dict
 
 
 class OrderService:
-    def __init__(self, db, parameters: dict):
+    def __init__(self, db, parameters: dict, output_context: dict):
         self.db = db
 
         order_id = parameters.get("order_id")
@@ -15,6 +17,13 @@ class OrderService:
         quantities = parameters.get("number")
         self.quantities: Optional[List[int]] = quantities if quantities else None
 
+        session_string = output_context[0].get("name")
+        self.session_id: str = (
+            extracting_session_id(session_string) if session_string else None
+        )
+
+        self.in_progress_order: dict = {}
+
     def complete_order(self):
         pass
 
@@ -22,10 +31,28 @@ class OrderService:
         pass
 
     def add_order(self):
-        if len(self.food_items) != len(self.quantities):
+        if (
+            len(self.food_items)
+            != len(self.quantities) | len(self.quantities)
+            == 0 | len(self.food_items)
+            == 0
+        ):
             fulfillment_text = "Sorry I didn't understand. Can you please specify food items and quantities."
         else:
-            fulfillment_text = f"Received {self.food_items} and {self.quantities}"
+            food_dict = dict(zip(self.food_items, self.quantities))
+
+            if self.session_id in self.in_progress_order:
+                current_food_dict = self.in_progress_order[self.session_id]
+                self.in_progress_order[self.session_id] = current_food_dict.update(
+                    self.food_dict
+                )
+            else:
+                self.in_progress_order[self.session_id] = food_dict
+
+            order_str = get_str_from_food_dict(self.in_progress_order[self.session_id])
+            fulfillment_text = (
+                f"So far you have: {order_str}. Do you need anything else?"
+            )
 
         return JSONResponse(content={"fulfillmentText": fulfillment_text})
 
