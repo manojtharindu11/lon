93a1b9c5871fa91aadcfa22295d3a94f9289f92d
manojtharindu11 <thilakarathnawatm.21@uom.lk>
2026-04-06 21:28:58 +0530

    Enhance DbConnect and OrderService with improved order handling, logging, and error management

--- app/db_connect.py
+++ app/db_connect.py
@@ -78,27 +78,77 @@ class DbConnect:
                     if food_item not in db_items:
                         raise ValueError(f"{food_item} not found")
 
+                cursor.execute("SELECT MAX(order_id) FROM orders")
+                max_order_id = cursor.fetchone()[0]
+
+                if max_order_id is None:
+                    order_id = 1
+                else:
+                    order_id = max_order_id + 1
+
                 for food_item, quantity in order_dict.items():
                     item_id, price = db_items[food_item]
 
-                cursor.execute(
-                    "INSERT INTO orders(item_id, quantity, total_price) VALUES (%s,%s,%s)",
-                    (item_id, quantity, price * quantity),
-                )
+                    cursor.execute(
+                        "INSERT INTO orders(order_id, item_id, quantity, total_price) VALUES (%s,%s,%s,%s)",
+                        (
+                            order_id,
+                            item_id,
+                            int(quantity),
+                            float(price) * float(quantity),
+                        ),
+                    )
 
-                order_id = cursor.lastrowid
-
-                logging.info(
-                    f"{food_item} - {quantity} = {quantity * price} added to database with order id: {order_id}"
-                )
+                    logging.info(
+                        f"{food_item} - {quantity} = {float(quantity) * float(price)} added to database."
+                    )
 
                 self.conn.commit()
-
                 return order_id
+
         except Exception as e:
             self.conn.rollback()
             logging.exception(f"Save order failed: {e}")
             return -1
+
+    def get_order_total(self, order_id: int) -> float:
+        self.ensure_connection()
+        try:
+            with self.conn.cursor() as curser:
+                curser.execute(
+                    """SELECT SUM(total_price)
+                       FROM orders
+                       WHERE order_id=%s
+                    """,
+                    (order_id,),
+                )
+
+                result = curser.fetchone()
+
+                if result is None:
+                    raise ValueError(f"Something went wrong with order id: {order_id}")
+
+                total_price = result[0]
+                return total_price
+        except Exception as e:
+            logging.exception(f"Database error: {e}")
+
+    def insert_order_tracking(self, order_id: int, status: str):
+        self.ensure_connection()
+        try:
+            with self.conn.cursor() as curser:
+                curser.execute(
+                    """INSERT INTO order_tracking(order_id, status)
+                       VALUES(%s, %s)
+                    """,
+                    (order_id, status),
+                )
+
+                self.conn.commit()
+
+        except Exception as e:
+            self.conn.rollback()
+            logging.exception(f"Database error: {e}")
 
 
 if __name__ == "__main__":
--- app/order_service.py
+++ app/order_service.py
@@ -1,4 +1,5 @@
 from typing import List, Optional
+from venv import logger
 from fastapi.responses import JSONResponse
 from app.generic_helper import extracting_session_id
 from app.generic_helper import get_str_from_food_dict
@@ -27,8 +28,10 @@ class OrderService:
     def complete_order(self):
         if self.session_id not in OrderService.in_progress_order:
             fulfillment_text = "I am having trouble finding your order. Sorry! Can you place a new order"
+            logger.warning(fulfillment_text)
+
         else:
-            order = OrderService.in_progress_order[self.order_id]
+            order = OrderService.in_progress_order[self.session_id]
             order_id = self.db.save_order(order)
 
             if order_id == -1:
@@ -36,17 +39,23 @@ class OrderService:
                     "Sorry, I couldn't process your order due to backend error."
                     "Please place a new order again"
                 )
+                logger.warning(fulfillment_text)
+
             else:
-                order_total = 10
+                order_total = self.db.get_order_total(order_id)
 
                 fulfillment_text = (
-                    "Awesome. We have place your order."
-                    f"Here is your {order_id}."
-                    f"Your order total is {order_total}"
+                    "Awesome. We have place your order. "
+                    f"Here is your order id:{int(order_id)}. "
+                    f"Your order total is Rs.{order_total}"
                 )
 
-    def save_order(self):
-        pass
+                logger.info(fulfillment_text)
+
+                del OrderService.in_progress_order[self.session_id]
+                self.db.insert_order_tracking(order_id, "in progress")
+
+        return JSONResponse(content={"fulfillmentText": fulfillment_text})
 
     def remove_order(self):
         pass
@@ -58,6 +67,7 @@ class OrderService:
             or len(self.food_items) == 0
         ):
             fulfillment_text = "Sorry I didn't understand. Can you please specify food items and quantities."
+            logger.warning(fulfillment_text)
         else:
             food_dict = dict(zip(self.food_items, self.quantities))
 
@@ -74,6 +84,8 @@ class OrderService:
                 f"So far you have: {order_str}. Do you need anything else?"
             )
 
+            logger.info(fulfillment_text)
+
         return JSONResponse(content={"fulfillmentText": fulfillment_text})
 
     def track_order(self):
@@ -86,4 +98,5 @@ class OrderService:
         else:
             fulfillment_text = f"No order found with order id: {self.order_id}"
 
+        logger.info(fulfillment_text)
         return JSONResponse(content={"fulfillmentText": fulfillment_text})
